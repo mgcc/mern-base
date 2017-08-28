ae7bb551730c33aeedbabff352ff9f6b1d4597ef
Monina Carandang <reactor04@gmail.com>
2017-08-28 13:34:58 +0800

    Refactored book functions into App

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 // Mongoose and DB
 const mongoose = require('mongoose');
@@ -14,7 +13,16 @@ mongoose.connect(DB_URL);
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+// for CORS
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
 
 // Models
 require('./models/Book');
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,68 +2,27 @@ import React, { Component } from 'react';
 
 export class BookForm extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      author: '',
-      title: ''
-    };
-
-    this.addBook = this.addBook.bind(this);
-    this.onChangeAuthor = this.onChangeAuthor.bind(this);
-    this.onChangeTitle = this.onChangeTitle.bind(this);
-  }
-
-  addBook() {
-    // const form = new FormData(document.getElementById('new-book'));
-    // console.log(form);
-    const data = {
-      author: this.state.author,
-      title: this.state.title
-    }
-
-    fetch('http://localhost:3001/api/book/add', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-    .then(
-      (res) => { console.log(res) },
-      (e) => { console.log(e) }
-    )
-    ;
-  }
-
-  onChangeAuthor(event) {
-    this.setState({ author: event.target.value });
-  }
-
-  onChangeTitle(event) {
-    this.setState({ title: event.target.value });
-  }
-
   render() {
 
+    const { author, title, onChangeTitle, onChangeAuthor, submitNewBook } = this.props;
+
     return (
-      <form id="new-form">
+      <form id="new-form" onSubmit={submitNewBook}>
         <input
           placeholder="title"
           type="text"
           name="title"
-          value={this.state.title}
-          onChange={this.onChangeTitle}
+          value={title}
+          onChange={onChangeTitle}
         />
         <input
           placeholder="author"
           type="text"
           name="author"
-          value={this.state.author}
-          onChange={this.onChangeAuthor}
+          value={author}
+          onChange={onChangeAuthor}
         />
-        <button type="button" onClick={this.addBook}>
+        <button type="submit">
           Add Book
         </button>
       </form>
